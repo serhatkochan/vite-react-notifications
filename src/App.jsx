6331a09d7cf5cd2dc12f0079ffcbbeb89325b46f
serhatkochan <serhatkochan@hotmail.com.tr>
2023-04-14 19:04:54 +0300

    init notification app

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import {useState} from 'react'
+import './App.css'
+import reactLogo from './assets/react.svg'
+
+function App() {
+    const getNotification = () => {
+        const title = "Notification title";
+        const body = "Notification body"
+        const icon = reactLogo;
+        if (!("Notification" in window)) {
+            alert("Tarayıcın desteklemiyor");
+        } else if (Notification.permission === 'denied') {
+            alert('Bildirime izin vermemişsin')
+        } else if (Notification.permission === 'granted') {
+            alertNotification(title, body, icon);
+        } else if (Notification.permission === 'default') {
+            Notification.requestPermission().then((permisson) => {
+                // izin alır, verilen izni döndürür
+                if (permisson === 'granted') {
+                    alertNotification(title, body, icon);
+                }
+            })
+        }
+    }
+    const alertNotification = (title, body, icon) => {
+        const notification = new Notification(title, {body, icon});
+        notification.notification.onclick = () => {
+            notification.close();
+            window.parent.focus();
+        }
+    }
+
+    return (
+        <div className="App">
+            <div className="card">
+                <button onClick={getNotification}>
+                    Anlık Bildirim
+                </button>
+                <button onClick={() => setTimeout(() => getNotification(), 3000)}>
+                    1 saniye sonra bildirim
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export default App
